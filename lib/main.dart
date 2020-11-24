5e99e13aa82741a998e266a4975a2df23ce04427
isaccanedo <isaccanedo@gmail.com>
2020-11-24 11:05:57 -0300

    :triangular_flag_on_post: Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+
+// ------------------------------------------------------
+// @author Isac Canedo
+// ------------------------------------------------------
+
+import 'package:flutter/material.dart';
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() {
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  String result = "Olá !";
+
+  Future _scanQR() async {
+    try {
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+      });
+    } on PlatformException catch (ex) {
+      if (ex.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          result = "A permissão da câmera foi negada";
+        });
+      } else {
+        setState(() {
+          result = "Erro desconhecido $ex";
+        });
+      }
+    } on FormatException {
+      setState(() {
+        result = "Você pressionou o botão Voltar antes de digitalizar qualquer coisa";
+      });
+    } catch (ex) {
+      setState(() {
+        result = "Erro desconhecido $ex";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QR Scanner"),
+      ),
+      body: Center(
+        child: Text(
+          result,
+          style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: _scanQR,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
